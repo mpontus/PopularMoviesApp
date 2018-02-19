1c9a74b3a51bec9ca19a6ccab74989572b592008
Mikhail Pontus <m.pontus@gmail.com>
2018-02-20 00:36:39 +0300

    Test API service

--- app/src/main/java/com/mpontus/popularmoviesapp/MainActivity.java
+++ app/src/main/java/com/mpontus/popularmoviesapp/MainActivity.java
@@ -2,6 +2,20 @@ package com.mpontus.popularmoviesapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +23,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        gsonBuilder.setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES);
+        gsonBuilder.setDateFormat("yyyy-MM-dd");
+        Gson gson = gsonBuilder.create();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://api.themoviedb.org/3/")
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .build();
+
+        TMDbService service = retrofit.create(TMDbService.class);
+
+        Call<MovieListResponse> call = service.getPopularMovies(BuildConfig.TMDB_API_KEY);
+
+        call.enqueue(new Callback<MovieListResponse>() {
+            @Override
+            public void onResponse(Call<MovieListResponse> call, Response<MovieListResponse> response) {
+                MovieListResponse result = response.body();
+
+                if (result == null) {
+                    return;
+                }
+
+                for (Movie movie : result.results) {
+                    Log.v("Movie", movie.title);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<MovieListResponse> call, Throwable t) {
+                Log.v("error", "error");
+            }
+        });
     }
 }

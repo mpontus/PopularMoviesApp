4d252edf7e1c6764c060efd58d9b2b581d19a0a2
Mikhail Pontus <m.pontus@gmail.com>
2018-02-28 01:58:36 +0300

    Introduce rx-preferences

--- app/src/main/java/com/mpontus/popularmoviesapp/MainActivity.java
+++ app/src/main/java/com/mpontus/popularmoviesapp/MainActivity.java
@@ -1,18 +1,26 @@
 package com.mpontus.popularmoviesapp;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+
+import com.f2prateek.rx.preferences2.Preference;
+import com.f2prateek.rx.preferences2.RxSharedPreferences;
 
 import javax.inject.Inject;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnItemSelected;
+import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.schedulers.Schedulers;
 
@@ -53,6 +61,8 @@ public class MainActivity extends AppCompatActivity implements MovieListAdapter.
      */
     private MovieListAdapter mMovieListAdapter = new MovieListAdapter(this);
 
+    Preference<Integer> mSortOrderPreference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -60,12 +70,15 @@ public class MainActivity extends AppCompatActivity implements MovieListAdapter.
         ButterKnife.bind(this);
         ((PopularMoviesApplication) getApplication()).getAppComponent().inject(this);
 
+        // Initialize movie sort order selector
         ArrayAdapter<CharSequence> sortOrderAdapter =
                 ArrayAdapter.createFromResource(this, R.array.movie_source_options,
                         android.R.layout.simple_spinner_dropdown_item);
 
         mSortOrderView.setAdapter(sortOrderAdapter);
 
+
+        // Initialize recycler view
         RecyclerView.LayoutManager movieListLayoutManager =
                 new GridLayoutManager(this, SPAN_COUNT);
 
@@ -73,6 +86,17 @@ public class MainActivity extends AppCompatActivity implements MovieListAdapter.
         mMovieListView.setLayoutManager(movieListLayoutManager);
         mMovieListView.setNestedScrollingEnabled(false);
 
+        // Test preferences
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        mSortOrderPreference = RxSharedPreferences.create(preferences)
+                .getInteger(getString(R.string.pref_sort_order_key));
+
+        mSortOrderPreference
+                .asObservable()
+                .subscribe(value -> Log.v("VALUE", String.valueOf(value)));
+
+        // Initialize service
         tmdbService.getPopularMovies()
                 .subscribeOn(Schedulers.newThread())
                 .observeOn(AndroidSchedulers.mainThread())
@@ -87,5 +111,26 @@ public class MainActivity extends AppCompatActivity implements MovieListAdapter.
         intent.putExtra(DetailActivity.EXTRA_MOVIE, m);
 
         startActivity(intent);
+    }
+
+    @OnItemSelected(R.id.spSortOrder)
+    public void onItemSelected(Spinner spinner, int position) {
+        int value = SORT_ORDER_VALUES[position];
+
+        Log.v("SETTING THE VALUE", String.valueOf(value));
+
+        switch (value) {
+            case SORT_ORDER_POPULAR:
+            case SORT_ORDER_RECENT:
+                PreferenceManager.getDefaultSharedPreferences(this)
+                        .edit()
+                        .putInt(getString(R.string.pref_sort_order_key), value)
+                        .apply();
+
+                return;
+
+            default:
+                throw new RuntimeException("Unexpected option: " + value);
+        }
     }
 }

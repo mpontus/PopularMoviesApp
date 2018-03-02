db4897d69ce0e6fdb221cf470415e881c2980c6e
Mikhail Pontus <m.pontus@gmail.com>
2018-03-02 20:40:41 +0300

    Handle edge cases

--- app/src/main/java/com/mpontus/popularmoviesapp/MainActivity.java
+++ app/src/main/java/com/mpontus/popularmoviesapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.mpontus.popularmoviesapp;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.os.PersistableBundle;
@@ -13,6 +16,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.f2prateek.rx.preferences2.Preference;
 
@@ -21,8 +25,12 @@ import javax.inject.Named;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import butterknife.OnItemSelected;
+import butterknife.Optional;
+import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.annotations.Nullable;
 import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity implements MovieListAdapter.OnClickListener {
@@ -51,12 +59,21 @@ public class MainActivity extends AppCompatActivity implements MovieListAdapter.
     /**
      * Movie category selector
      */
-    @BindView(R.id.spSortOrder) Spinner mSortOrderView;
+    @Nullable
+    @BindView(R.id.spSortOrder)
+    Spinner mSortOrderView;
 
     /**
      * Recycler view for movie listing
      */
-    @BindView(R.id.rvMovies) RecyclerView mMovieListView;
+    @Nullable
+    @BindView(R.id.rvMovies)
+    RecyclerView mMovieListView;
+
+    /**
+     * Saved recycler view position
+     */
+    private Bundle mSavedInstanceState;
 
     /**
      * Recycler view adapter
@@ -71,6 +88,22 @@ public class MainActivity extends AppCompatActivity implements MovieListAdapter.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mSavedInstanceState = savedInstanceState;
+
+        tryNetwork();
+    }
+
+    @Optional
+    @OnClick(R.id.btnTryNetwork)
+    protected void tryNetwork() {
+        if (!isNetworkAvailable()) {
+            setContentView(R.layout.activity_main_without_network);
+            ButterKnife.bind(this);
+
+            return;
+        }
+
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         ((PopularMoviesApplication) getApplication()).getAppComponent().inject(this);
@@ -101,20 +134,38 @@ public class MainActivity extends AppCompatActivity implements MovieListAdapter.
                     }
                 })
                 .observeOn(AndroidSchedulers.mainThread())
+                .doOnError(error -> {
+                    Toast.makeText(this, R.string.fetching_error_message, Toast.LENGTH_SHORT)
+                            .show();
+                })
+                .onErrorResumeNext(Observable.empty())
                 .subscribe(response -> {
                     mMovieListAdapter.setMovies(response.results);
 
-                    if (savedInstanceState == null) {
+                    if (mSavedInstanceState == null) {
                         return;
                     }
 
                     Parcelable movieListLayoutManagerState =
-                            savedInstanceState.getParcelable(SAVED_STATE_MOVIE_LIST_LAYOUT_MANAGER);
+                            mSavedInstanceState.getParcelable(SAVED_STATE_MOVIE_LIST_LAYOUT_MANAGER);
 
                     if (movieListLayoutManagerState != null) {
                         mMovieListLayoutManager.onRestoreInstanceState(movieListLayoutManagerState);
                     }
                 });
+    }
+
+    protected boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        if (connectivityManager == null) {
+            return false;
+        }
+
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
     }
 
     @Override
@@ -135,11 +186,10 @@ public class MainActivity extends AppCompatActivity implements MovieListAdapter.
         startActivity(intent);
     }
 
+    @Optional
     @OnItemSelected(R.id.spSortOrder)
     public void onItemSelected(Spinner spinner, int position) {
         int value = SORT_ORDER_VALUES[position];
-
-        Log.v("SETTING THE VALUE", String.valueOf(value));
 
         switch (value) {
             case SORT_ORDER_POPULAR:

a9843f75584051632b69b943ae348bb5b01c5513
Mikhail Pontus <m.pontus@gmail.com>
2018-03-03 17:55:07 +0300

    Refactor alternative layouts for no connectivity

--- app/src/main/java/com/mpontus/popularmoviesapp/MainActivity.java
+++ app/src/main/java/com/mpontus/popularmoviesapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.mpontus.popularmoviesapp;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
@@ -10,6 +13,7 @@ import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ProgressBar;
@@ -19,11 +23,13 @@ import android.widget.Toast;
 import com.f2prateek.rx.preferences2.Preference;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.inject.Inject;
 import javax.inject.Named;
 
 import butterknife.BindView;
+import butterknife.BindViews;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import butterknife.OnItemSelected;
@@ -33,7 +39,7 @@ import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.annotations.Nullable;
 import io.reactivex.schedulers.Schedulers;
 
-public class MainActivity extends AppCompatActivity implements MovieListAdapter.OnClickListener {
+public class MainActivity extends AppCompatActivity implements MovieListAdapter.OnClickListener, SharedPreferences.OnSharedPreferenceChangeListener {
 
     public static final int SORT_ORDER_POPULAR = 1;
     public static final int SORT_TOP_RATED = 2;
@@ -59,23 +65,38 @@ public class MainActivity extends AppCompatActivity implements MovieListAdapter.
     /**
      * Movie category selector
      */
-    @Nullable
     @BindView(R.id.spSortOrder)
     Spinner mSortOrderView;
 
     /**
      * Recycler view for movie listing
      */
-    @Nullable
     @BindView(R.id.rvMovies)
     RecyclerView mMovieListView;
 
     /**
      * Loading indicator
      */
-    @Nullable
     @BindView(R.id.progressBar)
     ProgressBar mLoadingIndicator;
+
+    /**
+     * Views to be visible when the phone is disconnected from network
+     */
+    @BindViews({ R.id.tvNoConnectionHeadline, R.id.tvNoConnectionSubheading })
+    List<View> mViewsOffline;
+
+    /**
+     * Views to be visible while fetching the data
+     */
+    @BindViews({ R.id.progressBar, R.id.spSortOrder })
+    List<View> mViewsFetching;
+
+    /**
+     * Views to be visible after the response is loaded
+     */
+    @BindViews({ R.id.spSortOrder, R.id.rvMovies})
+    List<View> mViewsLoaded;
 
     /**
      * Saved recycler view position
@@ -87,138 +108,80 @@ public class MainActivity extends AppCompatActivity implements MovieListAdapter.
      */
     private MovieListAdapter mMovieListAdapter = new MovieListAdapter(this);
 
-    @Inject
-    @Named("sort_order")
-    Preference<Integer> mSortOrderPreference;
+    /**
+     * Current sort order selection
+     */
+    int mSortOrder;
+
+    /**
+     * Current network status
+     */
+    boolean mIsOnline;
+
+    /**
+     * Flag to signify that we are waiting for network connetion to fetch movies
+     */
+    boolean mWaitingForNetwork = false;
+
+    /**
+     * Layout manager extracted in order to save recycler view position
+     */
     private RecyclerView.LayoutManager mMovieListLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Store saved instance state for use during initialization
-        mSavedInstanceState = savedInstanceState;
-
-        // Most of the initialization is delegated to separate method to allow the user to invoke
-        // reinitialization when the network is unavailable
-        tryNetwork();
-    }
-
-    @Optional
-    @OnClick(R.id.btnTryNetwork)
-    protected void tryNetwork() {
-        if (!isNetworkAvailable()) {
-            setContentView(R.layout.activity_main_without_network);
-            ButterKnife.bind(this);
-
-            return;
-        }
-
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        ((PopularMoviesApplication) getApplication()).getAppComponent().inject(this);
+        ((PopularMoviesApplication) getApplication()).getAppComponent()
+                .inject(this);
+
+        // Store saved instance state to be used after populating the movie list adapter
+        mSavedInstanceState = savedInstanceState;
+
+        // Synchronize mSortOrder with the value in shared preferences
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        mSortOrder = preferences.getInt(getString(R.string.pref_sort_order_key), SORT_ORDER_POPULAR);
+
+        preferences.registerOnSharedPreferenceChangeListener(this);
+
+        // Synchronize mIsOnline with current connectivity status
+        mIsOnline = isOnline();
+
+        registerReceiver(new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                mIsOnline = !intent.getBooleanExtra(ConnectivityManager.EXTRA_NO_CONNECTIVITY, false);
+
+                // Retry fetching the movies when connection is reestablished
+                if (mIsOnline && mWaitingForNetwork) {
+                    fetchMovies();
+                }
+            }
+        }, new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
 
         // Initialize movie sort order selector
         ArrayAdapter<CharSequence> sortOrderAdapter =
                 ArrayAdapter.createFromResource(this, R.array.movie_sort_order_options,
                         android.R.layout.simple_spinner_dropdown_item);
 
-
         mSortOrderView.setAdapter(sortOrderAdapter);
+        // TODO: Refactor sort order adapter
+        mSortOrderView.setSelection(mSortOrder == SORT_ORDER_POPULAR ? 0 : 1);
 
         // Initialize recycler view
         mMovieListLayoutManager = new GridLayoutManager(this, SPAN_COUNT);
 
         mMovieListView.setAdapter(mMovieListAdapter);
         mMovieListView.setLayoutManager(mMovieListLayoutManager);
-        mMovieListView.setNestedScrollingEnabled(false);
 
-        // Observable of preference changes, including the initial preference value
-        Observable<Integer> sortOrderPreferenceObservable = mSortOrderPreference
-                .asObservable();
-
-        // Observable of movie responses which follow preference changes
-        Observable<MovieListResponse> movieListResponseObservable = sortOrderPreferenceObservable
-                .observeOn(Schedulers.newThread())
-                .switchMap(sortOrder -> {
-                    if (sortOrder == SORT_ORDER_POPULAR) {
-                        return tmdbService.getPopularMovies();
-                    } else {
-                        return tmdbService.getTopRatedMovies();
-                    }
-                })
-                .observeOn(AndroidSchedulers.mainThread())
-                .doOnError(error -> {
-                    Toast.makeText(this, R.string.fetching_error_message, Toast.LENGTH_SHORT)
-                            .show();
-                })
-                .onErrorResumeNext(Observable.empty());
-
-        // Observable of booleans which singlify the loading state of the application
-        Observable<Boolean> isFetchingObservable = Observable.merge(
-                sortOrderPreferenceObservable.map(value -> true),
-                movieListResponseObservable.map(value -> false)
-        );
-
-        // Reset movie list before loading new results
-        sortOrderPreferenceObservable
-                .subscribe(value -> {
-                    mMovieListAdapter.setMovies(new ArrayList<>());
-                });
-
-        // Update movie list after the response
-        movieListResponseObservable
-                .subscribe(response -> {
-                    mMovieListAdapter.setMovies(response.results);
-                });
-
-        // Restore the position of RecyclerView after the first response
-        movieListResponseObservable
-                .firstElement()
-                .subscribe(value -> {
-                    if (mSavedInstanceState == null) {
-                        return;
-                    }
-
-                    Parcelable movieListLayoutManagerState =
-                            mSavedInstanceState.getParcelable(SAVED_STATE_MOVIE_LIST_LAYOUT_MANAGER);
-
-                    if (movieListLayoutManagerState != null) {
-                        mMovieListLayoutManager.onRestoreInstanceState(movieListLayoutManagerState);
-                    }
-
-                    // Probably useless because this subscription can only be initialized once.
-                    mSavedInstanceState = null;
-                });
-
-        // Show loading indicator instead of the UI while the response is loading
-        isFetchingObservable
-                .subscribe(isFetching -> {
-                    if (isFetching) {
-                        mMovieListView.setVisibility(View.INVISIBLE);
-                        mSortOrderView.setVisibility(View.INVISIBLE);
-                        mLoadingIndicator.setVisibility(View.VISIBLE);
-                    } else {
-                        mMovieListView.setVisibility(View.VISIBLE);
-                        mSortOrderView.setVisibility(View.VISIBLE);
-                        mLoadingIndicator.setVisibility(View.INVISIBLE);
-                    }
-                });
+        fetchMovies();
     }
 
-    protected boolean isNetworkAvailable() {
-        ConnectivityManager connectivityManager
-                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-
-        if (connectivityManager == null) {
-            return false;
-        }
-
-        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
-
-        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
-    }
-
+    /**
+     * Save the position of the recycler view
+     */
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         Parcelable movieListLayoutManagerState = mMovieListLayoutManager.onSaveInstanceState();
@@ -228,6 +191,9 @@ public class MainActivity extends AppCompatActivity implements MovieListAdapter.
         super.onSaveInstanceState(outState);
     }
 
+    /**
+     * Open detail activity when the movie is clicked
+     */
     @Override
     public void onClick(View v, Movie m) {
         Intent intent = new Intent(this, DetailActivity.class);
@@ -237,7 +203,9 @@ public class MainActivity extends AppCompatActivity implements MovieListAdapter.
         startActivity(intent);
     }
 
-    @Optional
+    /**
+     * Update sort order preference when user uses the spinner
+     */
     @OnItemSelected(R.id.spSortOrder)
     public void onItemSelected(Spinner spinner, int position) {
         int value = SORT_ORDER_VALUES[position];
@@ -255,5 +223,109 @@ public class MainActivity extends AppCompatActivity implements MovieListAdapter.
             default:
                 throw new RuntimeException("Unexpected option: " + value);
         }
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if (!key.equals(getString(R.string.pref_sort_order_key))) {
+            return;
+        }
+
+        mSortOrder = sharedPreferences.getInt(getString(R.string.pref_sort_order_key), SORT_ORDER_POPULAR);
+
+        // Fetch movies when the preference changes
+        fetchMovies();
+    }
+
+    /**
+     * Fetch the movies and populate the recycler view
+     */
+    protected void fetchMovies() {
+        if (!mIsOnline) {
+            ButterKnife.apply(mViewsFetching, setVisibility(View.GONE));
+            ButterKnife.apply(mViewsLoaded, setVisibility(View.GONE));
+            ButterKnife.apply(mViewsOffline, setVisibility(View.VISIBLE));
+
+            mWaitingForNetwork = true;
+
+            return;
+        }
+
+        mWaitingForNetwork = false;
+
+        ButterKnife.apply(mViewsOffline, setVisibility(View.GONE));
+        ButterKnife.apply(mViewsLoaded, setVisibility(View.GONE));
+        ButterKnife.apply(mViewsFetching, setVisibility(View.VISIBLE));
+
+        int sortOrder = PreferenceManager.getDefaultSharedPreferences(this)
+                .getInt(getString(R.string.pref_sort_order_key), SORT_ORDER_POPULAR);
+
+        Observable<MovieListResponse> movieListResponseObservable =
+                sortOrder == SORT_ORDER_POPULAR
+                        ? tmdbService.getPopularMovies()
+                        : tmdbService.getTopRatedMovies();
+
+        movieListResponseObservable
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnError(error -> {
+                    Toast.makeText(this, R.string.fetching_error_message, Toast.LENGTH_SHORT)
+                            .show();
+                })
+                .onErrorResumeNext(Observable.empty())
+                .subscribe(
+                        response -> {
+                            mMovieListAdapter.setMovies(response.results);
+
+                            // Restore recycler view position
+                            if (mSavedInstanceState != null) {
+                                Parcelable movieListLayoutManagerState = mSavedInstanceState
+                                        .getParcelable(SAVED_STATE_MOVIE_LIST_LAYOUT_MANAGER);
+
+                                if (movieListLayoutManagerState != null) {
+                                    mMovieListLayoutManager.onRestoreInstanceState(
+                                            movieListLayoutManagerState
+                                    );
+                                }
+
+                                mSavedInstanceState = null;
+                            }
+                        },
+                        error -> {
+                            Toast.makeText(
+                                    this,
+                                    R.string.fetching_error_message,
+                                    Toast.LENGTH_SHORT
+                            ).show();
+                        },
+                        () -> {
+                            ButterKnife.apply(mViewsFetching, setVisibility(View.GONE));
+                            ButterKnife.apply(mViewsLoaded, setVisibility(View.VISIBLE));
+                        }
+                );
+
+    }
+
+    /**
+     * Return network status
+     */
+    private boolean isOnline() {
+        ConnectivityManager connectivityManager = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        if (connectivityManager == null) {
+            return false;
+        }
+
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+
+        return networkInfo != null && networkInfo.isConnected();
+    }
+
+    /**
+     * Create ButterKnife action which sets View visibility to the given value
+     */
+    private ButterKnife.Action<View> setVisibility(int value) {
+        return (view, index) -> view.setVisibility(value);
     }
 }

b0cc216871b8bce34064c4ca95abe09b49fa0a7d
Mikhail Pontus <m.pontus@gmail.com>
2018-04-05 23:04:39 +0300

    Use shared movie list observable in Fragment Presenter

--- app/src/main/java/com/mpontus/popularmoviesapp/ui/MovieListFragment/MovieListFragmentPresenter.java
+++ app/src/main/java/com/mpontus/popularmoviesapp/ui/MovieListFragment/MovieListFragmentPresenter.java
@@ -3,88 +3,56 @@ package com.mpontus.popularmoviesapp.ui.MovieListFragment;
 import com.mpontus.popularmoviesapp.data.AppApiHelper;
 import com.mpontus.popularmoviesapp.data.AppConnectivityHelper;
 import com.mpontus.popularmoviesapp.di.FragmentScoped;
-import com.mpontus.popularmoviesapp.tmdb.TMDbService;
+import com.mpontus.popularmoviesapp.tmdb.Movie;
+
+import java.util.List;
 
 import javax.inject.Inject;
-import javax.inject.Named;
 
-import io.reactivex.Scheduler;
+import io.reactivex.Observable;
 import io.reactivex.disposables.CompositeDisposable;
 
 @FragmentScoped
 public class MovieListFragmentPresenter implements MovieListFragmentContract.Presenter {
-    private final AppApiHelper mApiHelper;
     private final AppConnectivityHelper mConnectivityHelper;
-    private final Scheduler mMainThreadScheduler;
     private final CompositeDisposable mCompositeDisposable;
+    private final Observable<List<Movie>> mMovieList;
 
     private MovieListFragmentContract.View mView;
-
-    private boolean mOnline;
-    private TMDbService.MovieSource mMovieSource;
-    private boolean mLoadWhenOnline = true;
-    private boolean mRequestPending = false;
 
     @Inject
     MovieListFragmentPresenter(MovieListFragmentContract.View view,
                                AppApiHelper repository,
                                AppConnectivityHelper networkStateHelper,
-                               @Named("MAIN") Scheduler mainThreadScheduler,
-                               TMDbService.MovieSource movieSource) {
+                               Observable<List<Movie>> movieList) {
         mView = view;
-        mApiHelper = repository;
         mConnectivityHelper = networkStateHelper;
-        mMainThreadScheduler = mainThreadScheduler;
         mCompositeDisposable = new CompositeDisposable();
-        mMovieSource = movieSource;
+        mMovieList = movieList;
     }
 
     public void attach() {
         mCompositeDisposable.add(
-                mConnectivityHelper.getIsOnline().subscribe(isOnline -> {
-                    mOnline = isOnline;
+                mConnectivityHelper.getIsOnline()
+                        .takeUntil(mMovieList)
+                        .subscribe(isOnline -> {
+                            if (isOnline) {
+                                mView.showLoading();
+                            } else {
+                                mView.showOffline();
+                            }
+                        })
+        );
 
-                    if (isOnline) {
-                        if (mLoadWhenOnline) {
-                            load(mMovieSource);
-                        }
-                    } else {
-                        if (mRequestPending) {
-                            mView.showOffline();
-                        }
-                    }
+        mCompositeDisposable.add(
+                mMovieList.subscribe(movies -> {
+                    mView.setMovieCount(movies.size());
+                    mView.showMovies();
                 })
         );
     }
 
     public void detach() {
         mCompositeDisposable.dispose();
-    }
-
-    private void load(TMDbService.MovieSource source) {
-        if (!mOnline) {
-            // Set the flag which will start loading when we come back online.
-            mLoadWhenOnline = true;
-
-            mView.showOffline();
-
-            return;
-        }
-
-        // Set the flag which will show offline screen if network goes offline during loading.
-        mRequestPending = true;
-
-        mView.showLoading();
-
-        mApiHelper.getMovies(source)
-                .observeOn(mMainThreadScheduler)
-                .subscribe(movies -> {
-                    mRequestPending = false;
-                    mLoadWhenOnline = false;
-
-                    mView.setMovieCount(movies.size());
-
-                    mView.showMovies();
-                });
     }
 }
